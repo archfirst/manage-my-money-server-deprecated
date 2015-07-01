7738bf2fd05f9cb39d03d5c1d16e1315369cac02
nareshbhatia <naresh.archfirst@gmail.com>
2015-07-01 16:37:46 -0400

    Replaced manual CORS initialization with CORS middleware

--- server/adapter/rest/api.js
+++ server/adapter/rest/api.js
@@ -7,25 +7,10 @@
 var express = require('express');
 var api = express();
 var bodyParser = require('body-parser');
-
-// Middleware to enable CORS
-var enableCORS = function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Credentials', true);
-    res.header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
-
-    // intercept OPTIONS method to simply send a 200 response
-    if (req.method === 'OPTIONS') {
-        res.status(200).send();
-    }
-    else {
-        next();
-    }
-};
+var cors = require('cors');
 
 // Add middleware to enable CORS
-api.use(enableCORS);
+api.use(cors());
 
 // Add middleware to parse the POST data of the body
 api.use(bodyParser.urlencoded({extended: true}));
